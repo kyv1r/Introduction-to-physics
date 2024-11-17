0cff4479b49cf5d5223697cf5af925a25bd7f2ed
Ilya <87643250+kyv1r@users.noreply.github.com>
2024-11-17 15:24:00 +1000

    transformations
    ДЗ: Трансформации 3.0

---
+++ Assets/Scripts/Resizer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Resizer : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.localScale += Vector3.one *_speed * Time.deltaTime;
+    }
+}
